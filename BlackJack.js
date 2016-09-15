bd451f5042bfb898844b61bb0e1cfec9520aa026
Richard Turner <richie.g.turner@gmail.com>
2016-09-15 18:12:57 -0400

    did some shit

--- BlackJack.js
+++ BlackJack.js
@@ -1,4 +1,20 @@
-console.log("hihi");
+var buttons = {
+  deal: 'deal',
+  hit: 'hitMe',
+  stand: 'stand'
+};
+
+var player = {
+  hand: [],
+  currentPoints: 0,
+  bankRoll: 1000,
+  currentBet: 0
+};
+
+var dealer = {
+  hand: [],
+  currentPoints: 0
+};
 
 var deck = [
   {
@@ -262,8 +278,10 @@ var deck = [
     gameValue: [1, 11]
   }
 ];
-//shuffle function taken from stack overflow
 
+var cardIndex = 0;
+
+//shuffle function taken from stack overflow
 function shuffle(a) {
     var j, x, i;
     for (i = a.length; i; i--) {
@@ -274,10 +292,68 @@ function shuffle(a) {
     }
 };
 
-shuffle(deck);
-console.log(deck);
+function deal() {
+  // Shuffle deck
+  shuffle(deck);
+  // Reset the hands of both dealer and player
+  cardIndex = 4;
+  dealer.hand = [];
+  player.hand = [];
+  // Insert cards into dealer and players hand array
+  player.hand.push(deck[0]);
+  player.hand.push(deck[2]);
+  dealer.hand.push(deck[1]);
+  dealer.hand.push(deck[3]);
+  // Calculate dealer and player hands
+  player.currentPoints = calcPointsForHand(player.hand);
+  dealer.currentPoints = calcPointsForHand(dealer.hand);
+  // Prompt user to hit or stay
+  console.log(player, dealer);
+}
 
-console.log(deck[0]);
+function calcPointsForHand(hand) {
+  var points = 0;
+  hand.forEach(function(card) {
+    points += card.gameValue[0];
+  });
+  return points;
+}
+
+// Takes either the player or dealer object as argument
+function hit(opponent) {
+  opponent.hand.push(deck[cardIndex]);
+  cardIndex++;
+  opponent.currentPoints = calcPointsForHand(opponent.hand);
+  // If opponents currentPoints is greater than 21 call gameOver function
+  if (opponent.currentPoints > 21) {
+    gameOver();
+  }
+  console.log(opponent.currentPoints);
+}
+
+// Game over function
+function gameOver() {
+  console.log('you lost!');
+}
+
+// Stand function
+function stand() {
+
+}
+
+// Dealers turn function
+function dealersTurn() {
+
+}
+
+// Attach event handlers
+document.getElementById(buttons.deal).addEventListener('click', function() {
+  deal();
+});
+
+document.getElementById(buttons.hit).addEventListener('click', function() {
+  hit(player);
+});
 
 //remove top card from array
 //look into splicing and slicing
